d3b3783ace1c3af7d46f32ba619eda2287e0fa57
Nagarjuna3e <148531051+Nagarjuna3e@users.noreply.github.com>
2025-01-01 21:12:57 +0530

    Add files via upload

--- PrimeSum.java
+++ PrimeSum.java
@@ -1,10 +1,8 @@
 package pratice;
-import java.util.Scanner;
+
 public class PrimeSum {
   public static void main(String[] args) {
-	  Scanner sc = new Scanner(Syatem.in);
-	  System.out.println("enter number ");
-	 int userinput=sc.nextInt();
+	 int userinput=100;
 	 int count=1;
 	 int num=2;
 	 int sum=0;
